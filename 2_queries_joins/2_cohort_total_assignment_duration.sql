d01b386b7e442ddf065c517327095e99b85fd522
masinette <masinette.ts@gmail.com>
2021-03-02 12:54:17 -0500

    create and seed teachers and assignment_submissions tables

--- 2_queries_joins/2_cohort_total_assignment_duration.sql
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -1,5 +1,5 @@
 SELECT SUM(duration)
-FROM assignment_submissions
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'FEB12';
+  FROM assignment_submissions
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = 'FEB12';
