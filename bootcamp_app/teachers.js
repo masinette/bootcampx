59c69d8d156df136aef1c7dd56ddba762f56bea6
masinette <masinette.ts@gmail.com>
2021-03-03 19:34:51 -0500

    refactor teacher and student js to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,17 +6,24 @@ const pool = new Pool({
 });
 
 
-
-pool.query(`
-SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+const queryString = `SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
-ORDER BY teacher;
-`)
+ORDER BY teacher
+LIMIT $2;`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher}`);
